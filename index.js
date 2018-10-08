381b0d069220c0d5b634366125e0d915fec8f9e1
echicken <echicken@bbs.electronicchicken.com>
2018-10-08 01:55:58 -0400

    Revised for ES6. Less of a scope & nesting nightmare. Code style somewhat more inline with my current practices. Requires node >= 6.4.0. Uses ssh2 module 0.6.1. No real functional change and no great need to update if your current install works properly.  This change is meant to make this thing easier for me to maintain & troubleshoot going forward. The Windows nexe executable has not been updated ... yet.

--- index.js
+++ index.js
@@ -1,121 +1,92 @@
+'use strict';
 const fs = require('fs');
 const net = require('net');
-const util = require('util');
 const path = require('path');
 const SSHClient = require('ssh2').Client;
 
-/*	Stupid nexe tricks
-	Delete 'settings.json' from the 'doorparty-connector' directory when
-	building with nexe; this will prevent the default settings file from being
-	included in the bundle.  A 'settings.json' file must exist in the working
-	directory of the executable, and will then be loaded at runtime. */
-try {
-	var settings = require(path.join(__dirname, './settings.json'));
-} catch (err) {
-	var settings = global.require('./settings.json');
+function on_tunnel(settings, tunnel, client, rlogin, user, err, stream) {
+  if (err) {
+    console.log('SSH tunnel error', err);
+    tunnel.end();
+    return;
+  }
+  console.log(`SSH tunnel to ${settings.SSHServer}:${settings.SSHPort} for ${user} established`);
+  client.on('data', data => stream.write(data, 'binary'));
+  stream.on('error', err => console.log('Stream error', err));
+  stream.on('data', data => client.write(data, 'binary'));
+  stream.on('close', () => tunnel.end());
+  console.log(`Connecting ${user} to ${settings.RLoginServer}:${settings.RLoginPort} via SSH tunnel`);
+  stream.write(rlogin, 'binary');
 }
 
-function onRlogin(client, rlogin) {
+function on_rlogin(settings, client, rlogin) {
 
-	var str = rlogin.toString().split(/\x00/);
+	let str = rlogin.toString().split(/\x00/);
 	if (str.length < 3) {
 		client.end();
 		return;
 	}
 
-	var sshTunnel = new SSHClient();
+	var tunnel = new SSHClient();
 
-	function onTunnel(err, stream) {
+	client.on('close', () => {
+		console.log(`Client session for ${str[2]} closed`);
+		tunnel.end();
+	});
 
-		if (err) {
-			console.log('SSH tunnel error', err);
-			sshTunnel.end();
-			return;
-		} else {
-			console.log(
-				util.format(
-					'SSH tunnel to %s:%s for %s established',
-					settings.SSHServer, settings.SSHPort, str[2]
-				)
-			);
-		}
+	tunnel.on('error', err => console.log(`SSH tunnel for ${str[2]} error`, err));
 
-		client.on(
-			'data', function (data) { stream.write(data, 'binary'); }
-		);
+  tunnel.on('close', () => {
+		console.log(`SSH Tunnel for ${str[2]} closed`);
+		client.end();
+  });
 
-		stream.on(
-			'error', function (err) { console.log('Stream error', err); }
-		);
-
-		stream.on(
-			'data', function (data) { client.write(data, 'binary'); }
-		);
-
-		stream.on('close', function () { sshTunnel.end(); });
-
-		console.log(
-			util.format(
-				'Connecting %s to %s:%s via SSH tunnel',
-				str[2], settings.RLoginServer, settings.RLoginPort
-			)
-		);
-
-		stream.write(rlogin, 'binary');
-
-	}
-
-	function onReady() {
-		sshTunnel.forwardOut(
-			settings.interface, settings.SSHPort,
+  tunnel.on('ready', () => {
+    tunnel.forwardOut(
+      settings.interface, settings.SSHPort,
 			settings.RLoginServer, settings.RLoginPort,
-			onTunnel
-		);
-	}
+			(err, stream) => {
+        on_tunnel(settings, tunnel, client, rlogin, str[2], err, stream);
+      }
+    )
+  });
 
-	client.on(
-		'close', function () {
-			console.log('Client session for ' + str[2] + ' closed');
-			sshTunnel.end();
-		}
-	);
-
-	sshTunnel.on(
-		'error', function (err) {
-			console.log('SSH tunnel for ' + str[2] + ' error', err);
-		}
-	);
-
-	sshTunnel.on(
-		'close', function () {
-			console.log('SSH Tunnel for ' + str[2] + ' closed');
-			client.end();
-		}
-	);
-
-	sshTunnel.on('ready', onReady);
-
-	sshTunnel.connect(
-		{	host: settings.SSHServer,
-			port: settings.SSHPort,
-			username: settings.username,
-			password: settings.password
-		}
-	);
+  tunnel.connect({
+    host: settings.SSHServer,
+		port: settings.SSHPort,
+		username: settings.username,
+		password: settings.password
+	});
 
 }
 
-function onClient(client) {
-	client.on('error', function (err) { console.log('Client error', err); });
-	client.once('data', function (data) { onRlogin(client, data); });
+function on_client(settings, client) {
+	client.on('error', err => console.log('Client error', err));
+	client.once('data', data => on_rlogin(settings, client, data));
 	console.log('Client connected');
 }
 
-const server = net.createServer(onClient);
-server.on('error', function (err) { console.log('Server error', err); });
-server.listen(
-	{ host : settings.interface, port : settings.port },
-	function () {
-		console.log('Listening on ' + settings.interface + ':' + settings.port);
-	}
-);
+function main() {
+
+  /**
+   * Delete 'settings.json' from 'doorparty-connector' directory when building
+   * with nexe; this will prevent inclusion of default settings file in the
+   * bundle.  'settings.json' must exist in the working directory of the
+   * executable, and will be loaded at runtime.
+   */
+  let settings;
+  try {
+  	settings = require(path.join(__dirname, './settings.json'));
+  } catch (err) {
+  	settings = global.require('./settings.json');
+  }
+
+  const server = net.createServer(client => on_client(settings, client));
+  server.on('error', err => console.log('Server error', err));
+  server.listen({ host : settings.interface, port : settings.port }, () => {
+  	console.log(`Listening on ${settings.interface}:${settings.port}`);
+  });
+
+}
+
+main();
